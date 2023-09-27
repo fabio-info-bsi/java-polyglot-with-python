701fb765880733fa4aa264e0e0e76527b4e62858
fabio.henrique <fabio.henrique@hotmart.com>
2023-09-27 06:29:29 -0300

    fix: add new examples in HelloWorld.java; remove comments in pom.xml; add exclude .gitignore;

--- src/main/java/br/com/fabex/HelloWorld.java
+++ src/main/java/br/com/fabex/HelloWorld.java
@@ -17,9 +17,23 @@ public class HelloWorld {
             URL urlScriptPython = HelloWorld.class
                     .getClassLoader()
                     .getResource(Paths.get("polyglot/python/src/script-utils.py").toString());
+
             ctx.eval(Source.newBuilder("python", urlScriptPython).build());
+            ctx.eval("python", "print('Hello world')");
+            ctx.eval("python", "name, nickName = 'Fabio', 'Fabex'");
+
+            final String PYTHON_SNIPPET_METHOD = """
+                    def methodHelloSnippet():
+                        print('Hello world')
+                    """;
+            ctx.eval("python", PYTHON_SNIPPET_METHOD);
+            ctx.getBindings("python").getMember("methodHelloSnippet").execute();
+
             Value method = ctx.getBindings("python").getMember("methodHello");
             method.execute();
+
+            Value member = ctx.getBindings("python").getMember("nickName");
+            System.out.println(member.asString());
         }
     }
 }
